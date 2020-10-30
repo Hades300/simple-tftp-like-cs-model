b9f1afb897b4425c7bc8a35c5c1baf88dba1ec9a
Hades300 <1018987488@qq.com>
2020-10-30 10:08:41 +0800

    format后，修改了上传的BUG 、
    Signed-off-by: Hades300 <1018987488@qq.com>

--- udpClient.py
+++ udpClient.py
@@ -1,103 +1,108 @@
 import queue
 from _socket import *
 import time
-import threading,struct
+import threading, struct
 
-PACKET_CODE ={
-    "write":1,
-    "read":2,
-    "list":3,
-    "data":4,
-    "ack":5
+PACKET_CODE = {
+    "write": 1,
+    "read": 2,
+    "list": 3,
+    "data": 4,
+    "ack": 5
 }
 
 CMD_CODE = {
-    1:"write",
-    2:"read",
-    3:"list",
-    4:"exit"
+    1: "write",
+    2: "read",
+    3: "list",
+    4: "exit"
 }
 
 
-def _build_rrq_packet(filename:str):
-    return struct.pack("1H1H%ds"%(len(filename.encode("utf8"))),PACKET_CODE["read"],len(filename.encode("utf8")),filename.encode("utf8"))
+def _build_rrq_packet(filename: str):
+    return struct.pack("1H1H%ds" % (len(filename.encode("utf8"))), PACKET_CODE["read"], len(filename.encode("utf8")),
+                       filename.encode("utf8"))
 
-def _build_wrq_packet(filename:str):
-    return struct.pack("1H1H%ds" % (len(filename.encode("utf8"))), PACKET_CODE["write"], len(filename.encode("utf8")), filename.encode("utf8"))
+
+def _build_wrq_packet(filename: str):
+    return struct.pack("1H1H%ds" % (len(filename.encode("utf8"))), PACKET_CODE["write"], len(filename.encode("utf8")),
+                       filename.encode("utf8"))
+
 
 def _build_lrq_packet():
-    return struct.pack("1H",PACKET_CODE["list"])
+    return struct.pack("1H", PACKET_CODE["list"])
 
-def _build_ack_packet(id:int):
-    return struct.pack("=1H1I",PACKET_CODE["ack"],id)
 
-def _build_data_packet(id:int,data:bytes):
+def _build_ack_packet(id: int):
+    return struct.pack("=1H1I", PACKET_CODE["ack"], id)
+
+
+def _build_data_packet(id: int, data: bytes):
     length = len(data)
-    return struct.pack(f"=1H1I{length}s",PACKET_CODE['data'],id,data)
+    return struct.pack(f"=1H1I{length}s", PACKET_CODE['data'], id, data)
 
 
-def _build_err_packet(e:Exception):
-    return _build_ack_packet(0,repr(e).encode("utf8"))
+def _build_err_packet(e: Exception):
+    return _build_ack_packet(0, repr(e).encode("utf8"))
 
-def download(c:socket,*args):
-    if len(args)!=1:
+
+def download(c: socket, *args):
+    if len(args) != 1:
         raise Exception("参数长度不对劲")
     filename = args[0]
     c.send(_build_rrq_packet(filename))
     data = c.recv(512)
     packet_code, = struct.unpack("1H", data[:2])
     if packet_code != (PACKET_CODE["ack"]):
-        raise Exception("下载请求未接受到对应ACK,接收到：",data.decode("utf8"))
-    with open(filename,"wb") as f:
+        raise Exception("下载请求未接受到对应ACK,接收到：", data.decode("utf8"))
+    with open(filename, "wb") as f:
         while True:
             data = c.recv(512)
-            packet_code, = struct.unpack("1H",data[:2])
-            if packet_code!=(PACKET_CODE["data"]):
-                raise Exception("期待一个data包,收到",data.decode("utf8"))
-            id, = struct.unpack("=1I",data[2:6])
+            packet_code, = struct.unpack("1H", data[:2])
+            if packet_code != (PACKET_CODE["data"]):
+                raise Exception("期待一个data包,收到", data.decode("utf8"))
+            id, = struct.unpack("=1I", data[2:6])
             f.write(data[6:])
             c.send(_build_ack_packet(id))
-            if (len(data[6:]))!=506:
+            if (len(data[6:])) != 506:
                 break
     print(f"[+] File {filename} Downloaded")
 
 
-
-
-
-def upload(c:socket,*args):
-    if len(args)!=2:
+def upload(c: socket, *args):
+    if len(args) != 2:
         raise Exception("参数长度不对劲")
     filename = args[0]
     serverfilename = args[1]
-    print("[DEBUG] SERVERNAME",serverfilename)
+    print("[DEBUG] SERVERNAME", serverfilename)
     c.send(_build_wrq_packet(serverfilename))
     data = c.recv(512)
     packet_code, = struct.unpack("1H", data[:2])
     if packet_code != (PACKET_CODE["ack"]):
-        raise Exception("下载请求未接受到对应ACK,接收到：",data.decode("utf8"))
+        raise Exception("下载请求未接受到对应ACK,接收到：", data.decode("utf8"))
     with open(filename, "rb") as f:
         id = 1
         while True:
             # 先不考虑最后一部分就刚好是506的情况
             content = f.read(506)
-            if len(content)<506:
+            if len(content) < 506:
                 # file end
                 packet = _build_data_packet(id, content)
                 c.send(packet)
                 break
             else:
-                packet = _build_data_packet(id,content)
+                packet = _build_data_packet(id, content)
                 c.send(packet)
                 # 此处开始 超时 计算
                 # 阻塞直到获取 ack
                 while True:
                     def warn():
                         raise Exception("超时")
-                    t= threading.Timer(1,function=warn)
+
+                    t = threading.Timer(1, function=warn)
                     t.start()
                     data = c.recv(512)
-                    packet_code, = struct.unpack("H",data[:2])
+                    packet_code, = struct.unpack("H", data[:2])
                     if packet_code == PACKET_CODE["ack"]:
                         t.cancel()
                         break
@@ -105,11 +110,12 @@ def upload(c:socket,*args):
                         raise Exception("期望是一个ACK 但不是")
                     # 超时重传
                     # c.send(packet)
-                id +=1
+                id += 1
     print(f"[+] {filename} uploaded")
     pass
 
-def get_list(c:socket):
+
+def get_list(c: socket):
     c.send(_build_lrq_packet())
     data = c.recv(512)
     packet_code, = struct.unpack("1H", data[:2])
@@ -121,39 +127,39 @@ def get_list(c:socket):
         packet_code, = struct.unpack("1H", data[:2])
         if packet_code != (PACKET_CODE["data"]):
             raise Exception("期待一个data包,收到", data.decode("utf8"))
-        content+=data[6:]
+        content += data[6:]
         if (len(data[6:])) != 506:
             break
     print("[+] File List ⤵️")
     print(content.decode("utf8"))
 
+
 def get_input():
     text = input("Hades300 >> :").split()
     while text[0].lower() not in CMD_CODE.values():
-        print("[+] Unsupported cmd %s"%text)
+        print("[+] Unsupported cmd %s" % text)
         text = input("Hades300 >> :").split()
     return text
 
+
 def connect(addr):
-    c = socket(AF_INET,SOCK_DGRAM)
+    c = socket(AF_INET, SOCK_DGRAM)
     c.connect(addr)
     cmd = get_input()
-    if len(cmd)==1 and cmd[0].lower()=="exit":
+    if len(cmd) == 1 and cmd[0].lower() == "exit":
         print("[+] Exiting... Bye~")
         c.close()
-    if cmd[0].lower()=="read":
+    if cmd[0].lower() == "read":
         print("[+] Downloading...")
-        download(c,*cmd[1:])
+        download(c, *cmd[1:])
     if cmd[0].lower() == "write":
         print("[+] Uploading...")
-        upload(c,*cmd[1:])
-    if cmd[0].lower()=="list":
+        upload(c, *cmd[1:])
+    if cmd[0].lower() == "list":
         get_list(c)
 
 
-if __name__=="__main__":
-    addr = ("localhost",8081)
+if __name__ == "__main__":
+    addr = ("localhost", 8081)
     while True:
         connect(addr)
-
-
--- udpServer.py
+++ udpServer.py
@@ -1,7 +1,7 @@
 import socketserver
-import struct,threading
+import struct, threading
 import time
-import socket,queue
+import socket, queue
 import typing
 import os
 
@@ -14,32 +14,37 @@ GUEST_UPLOADING = {}
 
 TIME_OUT = 2
 
-def guest_add(host:(str,int)):
+
+def guest_add(host: (str, int)):
     GUEST_LIST.append(host)
     GUEST_ACK[host] = queue.Queue(1)
 
-def guest_del(host:(str,int)):
+
+def guest_del(host: (str, int)):
     GUEST_LIST.remove(host)
     del GUEST_ACK[host]
 
-def guest_exist(host:(str,int)):
+
+def guest_exist(host: (str, int)):
     try:
         GUEST_LIST.index(host)
         return True
     except:
         return False
 
-def mark_acked(host:(str,int),id:int):
+
+def mark_acked(host: (str, int), id: int):
     GUEST_ACK[host].put(id)
 
-def mark_timeout(host:(str,int)):
+
+def mark_timeout(host: (str, int)):
     GUEST_ACK[host].put(False)
 
 
-def is_acked(host,id):
+def is_acked(host, id):
     if not guest_exist(host):
         return False
-    t= threading.Timer(1,function=mark_timeout,args=(host,))
+    t = threading.Timer(1, function=mark_timeout, args=(host,))
     t.start()
     res = GUEST_ACK[host].get()
     if res == id:
@@ -49,14 +54,14 @@ def is_acked(host,id):
         return False
 
 
-def on_file_piece_uploading(client: (socket.socket, (str, int)),data:bytes):
-    sock,addr = client
-    packet_code, = struct.unpack("=H",data[:2])
+def on_file_piece_uploading(client: (socket.socket, (str, int)), data: bytes):
+    sock, addr = client
+    packet_code, = struct.unpack("=H", data[:2])
     # 若得到的是wrq
-    if packet_code==PACKET_CODE["write"]:
-        file_name_length ,= struct.unpack("=H",data[2:4])
-        print("[length]",len(data[4:]))
-        filename,= struct.unpack(f"{file_name_length}s",data[4:])
+    if packet_code == PACKET_CODE["write"]:
+        file_name_length, = struct.unpack("=H", data[2:4])
+        print("[length]", len(data[4:]))
+        filename, = struct.unpack(f"{file_name_length}s", data[4:])
         filename = filename.decode("utf8")
         GUEST_UPLOADING[addr] = filename
         sock.sendto(_build_ack_packet(0), addr)
@@ -64,19 +69,21 @@ def on_file_piece_uploading(client: (socket.socket, (str, int)),data:bytes):
     # 不是wrq的话只能是data包
     if addr not in GUEST_UPLOADING.keys():
         raise Exception("当前无正在上传的文件")
-    id , = struct.unpack("1I",data[2:6])
-    sock.sendto(_build_ack_packet(id),addr)
+    id, = struct.unpack("1I", data[2:6])
+    sock.sendto(_build_ack_packet(id), addr)
     content = data[6:]
-    filename= GUEST_UPLOADING[addr]
-    if len(content)!=(512-6):
+    filename = GUEST_UPLOADING[addr]
+    filename = os.path.join(FILE_DIR,filename)
+    if len(content) != (512 - 6):
+        with open(filename, "ab") as f:
+            f.write(content)
         print(f"[+] File {GUEST_UPLOADING[addr]} uploaded")
         del GUEST_UPLOADING[addr]
     else:
-        filename = os.path.join(FILE_DIR,filename)
-        with open(filename,"ab") as f:
+        filename = os.path.join(FILE_DIR, filename)
+        with open(filename, "ab") as f:
             f.write(content)
     pass
-
 
 
 # single thread downloading
@@ -85,81 +92,79 @@ def download(client: (socket.socket, (str, int)), filename: str):
     guest_add(addr)
     if filename not in FILE_LIST:
         raise Exception(f"{filename} Not Exists,We have {FILE_LIST}")
-    filename = os.path.join(FILE_DIR,filename)
-    sock.sendto(_build_ack_packet(0),addr)
+    filename = os.path.join(FILE_DIR, filename)
+    sock.sendto(_build_ack_packet(0), addr)
     with open(filename, "rb") as f:
         id = 1
         while True:
             # 先不考虑最后一部分就刚好是506的情况
             content = f.read(506)
-            if len(content)<506:
+            if len(content) < 506:
                 # file end
                 packet = _build_data_packet(id, content)
                 sock.sendto(packet, addr)
                 break
             else:
-                packet = _build_data_packet(id,content)
-                sock.sendto(packet,addr)
+                packet = _build_data_packet(id, content)
+                sock.sendto(packet, addr)
                 # 此处开始 超时 计算
                 # 阻塞直到获取 ack
-                while is_acked(addr,id)==False:
+                while is_acked(addr, id) == False:
                     # 超时重传
                     sock.sendto(packet, addr)
-                id +=1
+                id += 1
     guest_del(addr)
     pass
 
+
 def list_files(client: (socket.socket, (str, int))):
-    sock,addr = client
-    sock.sendto(_build_ack_packet(0),addr)
+    sock, addr = client
+    sock.sendto(_build_ack_packet(0), addr)
     info = "\n".join([f"{i.name}  {i.stat().st_size}" for i in os.scandir(FILE_DIR)])
     data = info.encode("utf8")
     part = data[:506]
-    id =1
+    id = 1
     # 默认非满506 为结尾
     while True:
-        if len(part)==506:
-            sock.sendto(_build_data_packet(id,part),addr)
+        if len(part) == 506:
+            sock.sendto(_build_data_packet(id, part), addr)
         else:
             sock.sendto(_build_data_packet(id, part), addr)
             break
-PACKET_CODE ={
-    "write":1,
-    "read":2,
-    "list":3,
-    "data":4,
-    "ack":5
+
+
+PACKET_CODE = {
+    "write": 1,
+    "read": 2,
+    "list": 3,
+    "data": 4,
+    "ack": 5
 }
 
 CMD_CODE = {
-    1:"write",
-    2:"read",
-    3:"list"
+    1: "write",
+    2: "read",
+    3: "list"
 }
 
 
-def parse_packet(data:bytes):
-
-    pass
-
-
-
 def err_hook(request: (socket.socket, (str, int)), e: Exception):
-    sock,addr = request
-    sock.sendto(repr(e).encode("utf8"),addr)
+    sock, addr = request
+    sock.sendto(repr(e).encode("utf8"), addr)
 
-def _build_ack_packet(id:int):
-    return struct.pack("=1H1I",PACKET_CODE["ack"],id)
 
-def _build_data_packet(id:int,data:bytes):
-    print("[DEBUG] Build %d data Packet"%id)
+def _build_ack_packet(id: int):
+    return struct.pack("=1H1I", PACKET_CODE["ack"], id)
+
+
+def _build_data_packet(id: int, data: bytes):
+    print("[DEBUG] Build %d data Packet" % id)
     length = len(data)
-    return struct.pack(f"=1H1I{length}s",PACKET_CODE['data'],id,data)
-
-def _build_err_packet(e:Exception):
-    return _build_ack_packet(0,repr(e).encode("utf8"))
+    return struct.pack(f"=1H1I{length}s", PACKET_CODE['data'], id, data)
 
 
+def _build_err_packet(e: Exception):
+    return _build_ack_packet(0, repr(e).encode("utf8"))
 
 
 def init():
@@ -167,36 +172,38 @@ def init():
     res = os.path.exists(FILE_DIR)
     if not res:
         os.mkdir(FILE_DIR)
-    FILE_LIST = [i.name for i in os.scandir(FILE_DIR) ]
+    FILE_LIST = [i.name for i in os.scandir(FILE_DIR)]
     print(f"[+] Init FILE_DIR {FILE_DIR}")
     print(f"[+] Init FILE_LIST {FILE_LIST}")
+
 
 class MyUdpHandler(socketserver.BaseRequestHandler):
     def setup(self) -> None:
         init()
+
     def handle(self):
         data, sock = self.request
         # try:
-        packet_code, = struct.unpack("H",data[:2])
+        packet_code, = struct.unpack("H", data[:2])
         if packet_code not in PACKET_CODE.values():
-            raise Exception("接受到代码  %s命令不存在"%(type(packet_code)),PACKET_CODE.values())
+            raise Exception("接受到代码  %s命令不存在" % (type(packet_code)), PACKET_CODE.values())
         if packet_code == PACKET_CODE["read"]:
-            file_name_length, = struct.unpack("H",data[2:4])
-            filename, = struct.unpack("%ds"%file_name_length,data[4:4+file_name_length])
+            file_name_length, = struct.unpack("H", data[2:4])
+            filename, = struct.unpack("%ds" % file_name_length, data[4:4 + file_name_length])
             filename = filename.decode("utf8")
-            t = threading.Thread(target=download,args=((sock,self.client_address),filename))
+            t = threading.Thread(target=download, args=((sock, self.client_address), filename))
             t.start()
         if packet_code == PACKET_CODE["ack"]:
-            id, = struct.unpack("I",data[2:6])
-            mark_acked(self.client_address,id)
-            print("[ACKED] %d"%id)
+            id, = struct.unpack("I", data[2:6])
+            mark_acked(self.client_address, id)
+            print("[ACKED] %d" % id)
         if packet_code == PACKET_CODE["write"] or packet_code == PACKET_CODE["data"]:
-            on_file_piece_uploading((sock,self.client_address),data)
+            on_file_piece_uploading((sock, self.client_address), data)
         if packet_code == PACKET_CODE["list"]:
-            print("[DEBUG] LIST")
-            list_files((sock,self.client_address))
+            list_files((sock, self.client_address))
         # except Exception as e:
         #     err_hook((sock,self.client_address),e)
+
 
 if __name__ == '__main__':
     server = socketserver.ThreadingUDPServer(('127.0.0.1', 8081), MyUdpHandler)
